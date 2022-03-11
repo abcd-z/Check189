651d507da9636f41dc7a60a1a08db11a2fbfaa86
anmotilu <43264497+abcd-z@users.noreply.github.com>
2022-03-11 11:02:36 +0800

    Update Check189.py

--- Check189.py
+++ Check189.py
@@ -6,6 +6,29 @@ import requests
 import rsa
 import time
 
+import platform  
+##获取系统版本  开始
+def get_system_Platform():
+    print ('system and bit'.center(40,'-'))
+    print(platform.architecture())
+    print ('system and deatial'.center(40,'-'))
+    print(platform.platform())
+    print ('system'.center(40,'-'))
+    print(platform.system())
+    print ("python Version".center(40,'-'))
+    print(platform.python_version())
+
+def myPlatform():
+  sysstr = platform.system()
+  if(sysstr =="Windows"):
+    print ("my platform is:"+"Windows platform".center(40,'-'))
+  elif(sysstr == "Linux"):
+    print ("my platform is:"+"Linux platform".center(40,'-'))
+  else:
+    print ("my platform is:"+"Others platform".center(40,'-'))
+get_system_Platform()
+myPlatform()
+##获取系统版本  结束
 s = requests.Session()
 
 username = ""
